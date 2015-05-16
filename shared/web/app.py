86c50bae9f57ca9c7f7833509ad5e9a07d43f34a
madflow <madflow@localhost>
2015-05-16 15:45:29 +0200

    Restarting and errors

--- shared/web/app.py
+++ shared/web/app.py
@@ -2,21 +2,59 @@ from bottle import Bottle, run, template, static_file, request
 import daemon
 import os
 import ConfigParser
+import subprocess
+import sys
 
 WEB_DIR = os.path.dirname(os.path.realpath(__file__))
-CONFIG_DIR = os.path.realpath(os.path.join(WEB_DIR,'../config')
-                             )
+CONFIG_DIR = os.path.realpath(os.path.join(WEB_DIR, '../config'))
+SHARED_DIR = os.path.realpath(os.path.join(WEB_DIR, '../'))
+
 app = Bottle()
 
-config = ConfigParser.SafeConfigParser()
-config.read(os.path.join(CONFIG_DIR, 'radicale.conf'))
+
+def execute(command):
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    while True:
+        nextline = process.stdout.readline()
+        if nextline == '' and process.poll() is not None:
+            break
+        sys.stdout.write(nextline)
+        sys.stdout.flush()
+
+    output = process.communicate()[0]
+    exit_code = process.returncode
+
+    if exit_code == 0:
+        return output
+    else:
+        raise Exception(command, exit_code, output)
+
 
 @app.route('/')
 def index():
-    return template('index.html', config=config)
+    messages = []
+
+    config = ConfigParser.SafeConfigParser()
+    files = config.read(os.path.join(CONFIG_DIR, 'radicale.conf'))
+
+    if len(files) == 0:
+        messages.append(('Configuration file could not be opened.', 'alert'))
+        return template('error.html', messages=messages)
+
+    return template('index.html', config=config, messages=messages)
+
 
 @app.post('/')
 def save():
+    messages = []
+    config = ConfigParser.SafeConfigParser()
+    files = config.read(os.path.join(CONFIG_DIR, 'radicale.conf'))
+
+    if len(files) == 0:
+        messages.append(('Configuration file could not be opened.', 'alert'))
+        return template('error.html', messages=messages)
+
     config.set('server', 'hosts', request.forms.get('radicale_server_hosts'))
     config.set('server', 'ssl', str(request.forms.get('radicale_server_ssl', False)))
     config.set('server', 'certificate', request.forms.get('radicale_server_certificate'))
@@ -27,22 +65,38 @@ def save():
     config.set('encoding', 'request', request.forms.get('radicale_server_encoding_request'))
     config.set('encoding', 'stock', request.forms.get('radicale_server_encoding_stock'))
 
-    with open(os.path.join(CONFIG_DIR, 'new.conf'), 'wb') as configfile:
-        config.write(configfile)
+    config.set('auth', 'htpasswd_filename', request.forms.get('radicale_auth_htpasswd_filename'))
+
+    config.set('rights', 'file', request.forms.get('radicale_rights_file'))
+
+    config.set('storage', 'filesystem_folder', request.forms.get('radicale_storage_filesystem_folder'))
+
+    try:
+        with open(os.path.join(CONFIG_DIR, 'radicale.conf'), 'wb') as configfile:
+            config.write(configfile)
+    except:
+        messages.append(('Could not save the configuration file', 'alert'))
 
     # Restarting Radicale
+    try:
+        execute(os.path.join(SHARED_DIR, 'radicale.sh') + ' restart_radicale')
+        messages.append(('Radicale restarted', 'success'))
+    except:
+        messages.append(('Restart Failed', 'alert'))
 
+    return template('index.html', config=config, messages=messages)
 
-    return template('index.html', config=config)
 
 def main():
-    run(app, host='0.0.0.0', port=5000, debug=True, use_reloader=True)
+    run(app, host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+
 
 @app.route('/static/<filepath:path>')
 def server_static(filepath):
     return static_file(filepath, root=os.path.join(WEB_DIR, 'static'))
 
+
 if __name__ == "__main__":
-    main()
-    #with daemon.DaemonContext(working_directory=WEB_DIR):
-    #    main()
+    # main()
+    with daemon.DaemonContext(working_directory=WEB_DIR):
+        main()
